6f521f811b6b5853f79f555f1590243a96756b15
Angel <angelde820@gmail.com>
2025-02-24 15:56:28 -0500

    Cree lista de tareas, borra componentes autogenerados, etc

---
+++ src/components/SideBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="SideBar">
+        <h2>Barra lateral</h2>
+        <ul>
+        <li><Router-Link to="/Contador">Contador</Router-Link></li>
+        <li><Router-Link to="/lista-de-tareas">Lista de tareas</Router-Link></li>
+        </ul>
+
+    </div>
+</template>
+ 
+
+<script setup>
+
+</script>
+
+
+<style>
+.SideBar {
+    width: 200px;
+    background-color: chocolate;
+    padding: 20px;
+}
+</style>
